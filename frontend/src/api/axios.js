3726d7fc67dbb773a3f9b7670e5f1085a193b819
CoCav <cavignaux.constance@gmail.com>
2026-04-11 15:56:41 -0400

    fix(api): properly attach JWT token in axios interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
+import { getToken } from "../utils/token";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+    const token = getToken();
 
-    if (token) {
+    if (token && config.headers) {
         config.headers.Authorization = `Bearer ${token}`;
     }
 

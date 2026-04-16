899efcdedc781bcac0a654b1bd4db5d97f771d9e
CoCav <cavignaux.constance@gmail.com>
2026-04-16 15:54:01 -0400

    setup global CSS structure (theme, base, layout, components, helpers) and introduce reusable UI components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/authProvider.jsx";
+import './styles/theme.css'
+import './styles/base.css'
+import './styles/layout.css'
+import './styles/components.css'
+import './styles/helpers.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
